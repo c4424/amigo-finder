d25434f8c2c71331c1437e25db364ae3ba9e0e39
c4424 <c4424@live.com>
2019-06-18 12:48:57 -0700

    objects, javascript added

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+[{"name":"Bruce Wayne","photo":"https://upload.wikimedia.org/wikipedia/en/8/87/Batman_DC_Comics.png","scores":["5","5","5","5","5","5","5","5","5","4"]},
+{"name":"Clark Kent","photo":"https://upload.wikimedia.org/wikipedia/en/e/eb/SupermanRoss.png","scores":["4","5","4","5","4","5","4","5","4","5"]},
+{"name":"Wonder Woman","photo":"https://upload.wikimedia.org/wikipedia/en/9/93/Wonder_Woman.jpg","scores":["3","3","3","3","3","3","3","3","3","3"]},
+{"name":"Hal Jordan","photo":"https://upload.wikimedia.org/wikipedia/en/2/2c/Green_Lantern_JLA_Secret_Origins.png","scores":["3","4","3","3","4","4","4","3","3","4"]},
+{"name":"Barry Allen","photo":"https://upload.wikimedia.org/wikipedia/en/7/7e/The_Flash_%28Barry_Allen%29_Alex_Ross%27s_art.jpg","scores":["5","5","4","5","5","5","5","5","5","5"]},
+{"name":"Arthur Curry","photo":"https://upload.wikimedia.org/wikipedia/en/9/9d/Aquaman_Rebirth_1.png","scores":["4","4","4","4","4","4","4","4","4","4"]},
+{"name":"Victor Stone","photo":"https://upload.wikimedia.org/wikipedia/en/5/58/Cyborg_%28Victor_Stone%29.jpg","scores":["2","3","3","2","2","3","2","3","3","2"]},
+{"name":"Clancy Brown","photo":"https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/LexLuthor1.png/250px-LexLuthor1.png","scores":["1","2","1","1","1","2","2","2","1","2"]},
+{"name":"Mark Hamill","photo":"https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28DC_Comics_character%29.jpg","scores":["1","1","1","1","1","1","1","1","1","1"]},
+{"name":"Oliver Queen","photo":"https://upload.wikimedia.org/wikipedia/en/2/22/Green_Arrow_%28Stephen_Amell%29.png","scores":["2","2","2","2","2","2","2","2","2","2"]}]
