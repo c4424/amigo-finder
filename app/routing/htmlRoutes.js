27aa297ffb8ede6b1479563d0b8cc20f4b54d636
c4424 <c4424@live.com>
2019-06-18 12:47:44 -0700

    htmlroutes added

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+// HTML Routes
+// =============================================================
+
+module.exports = function(app, path) {
+
+	app.get("/survey", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+	
+	app.get("*", function(req, res) {
+	  res.sendFile(path.join(__dirname, "../public/index.html"));
+	});
+}
