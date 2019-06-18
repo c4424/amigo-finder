4431719de784f0506bad1a9d823b48461d8605e5
c4424 <c4424@live.com>
2019-06-18 12:44:48 -0700

    server, npm, router

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Static files
+app.use(express.static('app/public'));
+
+
+// Router
+//require('./app/routing/apiRoutes.js')(app, path);
+//require('./app/routing/htmlRoutes.js')(app, path);
+
+
+app.listen(PORT, function() {
+    console.log("I\'m listening... on port " + PORT);
+})
